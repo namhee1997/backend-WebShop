d4582be35876f3cb7d372097305359b12acc01e5
Nam-Nee <namkaka2612@gmail.com>
2022-07-07 09:10:55 +0700

    update package

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ const dbUser = require('./model/User');
 const dbUserOrder = require('./model/OrderUser');
 const dbProduct = require('./model/Product');
 const bcrypt = require('bcryptjs');
-const bodyParser = require('body-Parser')
+// const bodyParser = require('body-Parser')
 const app = express();
 
 app.use(cors());
 app.use(cookieParser());
 //Bodyparser Middleware
-app.use(bodyParser.json())
-app.use(express.json())
+// app.use(bodyParser.json())
+// app.use(express.json())
 // app.use(express.json({ limit: '50mb' }));
 // app.use(express.urlencoded({ limit: '50mb' }));
 
